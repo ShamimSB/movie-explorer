d3978cd2c3db302e52e0ff4a1e7fe6d901750d7d
ShamimSB <bhuiyanshamim09@gmail.com>
2026-09-17 19:45:03 +0600

    Fix typo in footer rel attribute

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="bg-gray-900 text-gray-400 text-center border-t border-gray-800 p-8">
       <p className="text-white font-semibold mb-4">MovieExplorer</p>
       
-      <a href="https://github.com/ShamimSB" target="_blank" rel="nooppener noreferrer" className="text-gray-400 hover:text-white transition underline">@shamimbhuiyan</a>
+      <a href="https://github.com/ShamimSB" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition underline">@shamimbhuiyan</a>
     </div>
   )
 }
